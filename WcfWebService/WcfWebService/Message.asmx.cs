6fd91b5d928f247b2438f6843b9a7274420dc6cb
weto <weto.jc@gmail.com>
2019-04-07 17:20:06 -0300

    Add serviço de envio de mensagem pelo webservice

---
+++ WcfWebService/WcfWebService/Message.asmx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WcfWebService
+{
+    /// <summary>
+    /// Summary description for Message
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Message
+    {
+        [WebMethod]
+        public List<Msg> SendMessage()
+        {
+           return new MessageRepository().Get();
+        }
+    }
+}
